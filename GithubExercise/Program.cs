74dd9328b9e790bc5435ff7284b695d786dcbed9
David Vallin <dv.code@outlook.com>
2021-10-13 20:53:05 +0200

    Added bear

--- GithubExercise/Program.cs
+++ GithubExercise/Program.cs
@@ -5,7 +5,8 @@ using System.Collections.Generic;
 List<Animal> animals = new List<Animal>()
 {
     new Duck(),
-    new Horse()
+    new Horse(),
+    new Bear()
 };
 
 foreach (Animal animal in animals)
